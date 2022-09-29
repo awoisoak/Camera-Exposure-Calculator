be67135fa5984640a9f93cef7707b04c89997c19
awoisoak <awoisoak@gmail.com>
2022-09-29 23:27:37 +0900

    Update target sdk version & migrate to androidx

--- app/src/main/java/com/awoisoak/exposure/presentation/ChronometerAsyncTask.java
+++ app/src/main/java/com/awoisoak/exposure/presentation/ChronometerAsyncTask.java
@@ -2,7 +2,7 @@ package com.awoisoak.exposure.presentation;
 
 import android.os.AsyncTask;
 import android.os.CountDownTimer;
-import android.support.constraint.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
